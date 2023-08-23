0f88bf6f895628f2f1a143128cc09f46ca390be7
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-23 14:45:28 -0300

    Exercício 3 resolvido

--- Calculadora/Exercicio3/Program.cs
+++ Calculadora/Exercicio3/Program.cs
@@ -4,7 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        float salarioBruto, adicionalNoturno, horasExtras, descontos;
+        
+        Console.WriteLine("Digite o salário bruto: ");
+        salarioBruto = float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Digite o valor do adicional noturno: ");
+        adicionalNoturno = float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Digite a quantidade de horas extras: ");
+        horasExtras = float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Digite o valor dos descontos: ");
+        descontos = float.Parse(Console.ReadLine());
+        
+        float salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
+        
+        Console.WriteLine($"\nSalário líquido: {salarioLiquido.ToString("C")}");
     }
 }
 
