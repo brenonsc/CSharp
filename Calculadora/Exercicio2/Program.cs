d86006dfa08d52c15cfd9b8415eb42153687af09
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-23 14:38:19 -0300

    Exercício 2 resolvido

--- Calculadora/Exercicio2/Program.cs
+++ Calculadora/Exercicio2/Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        float[] notas = new float[4];
+        float soma = 0;
         
+        for(int i=0; i < notas.Length; i++)
+        {
+            Console.WriteLine($"Digite a {i+1}ª nota: ");
+            notas[i] = float.Parse(Console.ReadLine());
+            
+            soma += notas[i];
+        }
+        
+        float media = soma / notas.Length;
+        
+        Console.WriteLine($"\nMédia: {media.ToString("N1")}");
     }
 }
 
