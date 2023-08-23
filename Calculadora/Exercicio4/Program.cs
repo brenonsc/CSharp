d7122f36071fedce2480d9b5733dda4269a787db
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-23 15:02:39 -0300

    Exercício 4 resolvido

--- Calculadora/Exercicio4/Program.cs
+++ Calculadora/Exercicio4/Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        float[] numeros = new float[4];
+        float diferenca;
+        
+        for(int i=0; i < numeros.Length; i++)
+        {
+            Console.WriteLine($"Digite o {i+1}° número: ");
+            numeros[i] = float.Parse(Console.ReadLine());
+        }
+        
+        diferenca = (numeros[0]*numeros[1]) - (numeros[2]*numeros[3]);
+        
+        Console.WriteLine($"\nDiferença: {diferenca.ToString("N1")}");
     }
 }
 
