658ea702c5c0d42d62af3c273c2c71a5030ba691
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-24 15:24:32 -0300

    Exercício 2 resolvido

---
+++ PlanoDeSaude/Exercicio2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercicio2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero;
+        
+        Console.Write("Digite um número: ");
+        numero = int.Parse(Console.ReadLine());
+        
+        if (numero % 2 == 0)
+        {
+            Console.Write($"\nO número {numero} é par ");
+        }
+        else
+        {
+            Console.Write($"\nO número {numero} é ímpar ");
+        }
+
+        if (numero > 0)
+        {
+            Console.Write("e positivo!");
+        }
+        else if(numero == 0)
+        {
+            Console.Write("e neutro!");
+        }
+        else
+        {
+            Console.Write("e negativo!");
+        }
+    }
+}
+
