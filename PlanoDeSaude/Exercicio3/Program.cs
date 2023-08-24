750259a90ed05f23fb9a74ef2f4230b077cf8030
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-24 15:36:05 -0300

    Exercício 3 resolvido

---
+++ PlanoDeSaude/Exercicio3/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exercicio3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nome;
+        int idade;
+        Boolean primeiraDoacao = true;
+        
+        Console.Write("Digite seu nome: ");
+        nome = Console.ReadLine();
+        
+        Console.Write("Digite sua idade: ");
+        idade = int.Parse(Console.ReadLine());
+
+        if (idade >= 60 && idade < 70)
+        {
+            Console.Write("Você já doou sangue alguma vez? (S/N): ");
+            if (Console.ReadLine().ToUpper() == "S")
+            {
+                primeiraDoacao = false;
+            }
+        }
+        
+        if(idade < 0)
+        {
+            Console.WriteLine("\nIdade inválida!");
+        }
+        else if (idade < 18)
+        {
+            Console.WriteLine($"\n{nome} não está apto a doar sangue!");
+        }
+        else if (idade > 18 && idade < 60)
+        {
+            Console.WriteLine($"\n{nome} está apto a doar sangue!");
+        }
+        else if (idade >= 60 && idade < 70 && primeiraDoacao == false)
+        {
+            Console.WriteLine($"\n{nome} está apto a doar sangue!");
+        }
+        else
+        {
+            Console.WriteLine($"\n{nome} não está apto doar sangue!");
+        }
+    }
+}
+
