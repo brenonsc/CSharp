f675fd41cac494314e62043a4efa90b6f4e18b2e
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-24 15:11:29 -0300

    Exercício 1 resolvido.

---
+++ PlanoDeSaude/Exercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b, c;
+        
+        Console.Write("Digite o número A: ");
+        a = int.Parse(Console.ReadLine());
+        
+        Console.Write("Digite o número B: ");
+        b = int.Parse(Console.ReadLine());
+        
+        Console.Write("Digite o número C: ");
+        c = int.Parse(Console.ReadLine());
+
+        if (a + b > c)
+            Console.WriteLine($"\nA soma de A({a}) e B({b}) é maior que C({c}).");
+        else if (a + b == c)
+            Console.WriteLine($"\nA soma de A({a}) e B({b}) é igual a C({c}).");
+        else
+            Console.WriteLine($"\nA soma de A({a}) e B({b}) é menor que C({c}).");
+    }
+}
+
