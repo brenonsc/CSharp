89d30cbee0c23e90f4bbe958d39ae2f3d8c8f36e
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-25 12:41:21 -0300

    Exercício 5 finalizado

---
+++ Tabuada/Exercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero, somaPositivos = 0;
+
+        do
+        {
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                somaPositivos += numero;
+            }
+        }
+        while (numero != 0);
+        
+        Console.Write($"\nA soma dos números positivos é: {somaPositivos}.");
+    }
+}
+
