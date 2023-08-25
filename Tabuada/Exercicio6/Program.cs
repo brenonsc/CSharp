1dae32fd29bdba561a5cc3f412399f97d96e430e
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-25 12:47:45 -0300

    Exercício 6 finalizado

---
+++ Tabuada/Exercicio6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero, somaMultiplos3 = 0;
+
+        do
+        {
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if(numero % 3 == 0)
+            {
+                somaMultiplos3 += numero;
+            }
+        }
+        while (numero != 0);
+        
+        Console.Write($"\nA soma dos números múltiplos de 3 é: {somaMultiplos3}.");
+    }
+}
+
