315d897c5c36a14d72c32e518aa120d4d94e5bce
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-25 11:24:57 -0300

    Exercício 2 resolvido

---
+++ Tabuada/Exercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int par = 0, impar = 0;
+        
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"Digite o {i}º número: ");
+            if (int.Parse(Console.ReadLine()) % 2 == 0)
+            {
+                par += 1;
+            }
+            else
+            {
+                impar += 1;
+            }
+        }
+        
+        Console.WriteLine($"\n---------------------------------\nTotal de números pares: {par}\nTotal de números ímpares: {impar}\n---------------------------------");
+    }
+}
+
