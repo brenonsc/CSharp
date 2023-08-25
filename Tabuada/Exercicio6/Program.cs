12ba2ac08d2935018af06f9eaf30378a8b1c7a21
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-25 14:49:21 -0300

    Exercício 6 corrigido

--- Tabuada/Exercicio6/Program.cs
+++ Tabuada/Exercicio6/Program.cs
@@ -4,21 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        int numero, somaMultiplos3 = 0;
+        float numero, somaMultiplos3 = 0, quantidade = 0;
 
         do
         {
             Console.Write("Digite um número: ");
-            numero = int.Parse(Console.ReadLine());
+            numero = float.Parse(Console.ReadLine());
 
-            if(numero % 3 == 0)
+            if(numero % 3 == 0 && numero != 0)
             {
+                quantidade += 1;
                 somaMultiplos3 += numero;
             }
         }
         while (numero != 0);
         
-        Console.Write($"\nA soma dos números múltiplos de 3 é: {somaMultiplos3}.");
+        Console.Write($"\nA média da soma dos números múltiplos de 3 é: {(somaMultiplos3/quantidade).ToString("F1")}.");
     }
 }
 
