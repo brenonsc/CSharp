2c6743c7de12cae3a5dfb9d2a5b4494069cd8da5
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-28 13:44:40 -0300

    Exercício 3 resolvido

---
+++ Vetores/Exercicio3/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Exercicio3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matriz = new int[3,3];
+        int somaPrincipal = 0, somaSecundaria = 0;
+        
+        for(int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for(int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.Write($"Matriz [{i}][{j}] = ");
+                matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        
+        Console.WriteLine("\nDiagonal principal:");
+        for(int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for(int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if (i == j)
+                {
+                    Console.Write($"{matriz[i, j]} ");
+                    
+                    somaPrincipal += matriz[i, j];
+                }
+            }
+        }
+        
+        Console.WriteLine("\n\nDiagonal secundária:");
+        for(int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for(int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if (i + j == matriz.GetLength(0) - 1)
+                {
+                    Console.Write($"{matriz[i, j]} ");
+
+                    somaSecundaria += matriz[i, j];
+                }
+            }
+        }
+        
+        Console.Write($"\n\nSoma dos elementos da diagonal principal: {somaPrincipal}\nSoma dos elementos da diagonal secundária: {somaSecundaria}");
+    }
+}
+
