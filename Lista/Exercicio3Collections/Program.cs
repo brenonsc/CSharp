850c4b0373e41898888d6ffa275893851ff5057e
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-08-29 14:56:44 -0300

    Exercício 3 Collections resolvido

---
+++ Lista/Exercicio3Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio3Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> setNumeros = new HashSet<string>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Digite o {i+1}º número: ");
+            setNumeros.Add(Console.ReadLine());
+        }
+        
+        Console.WriteLine("\nOs números cadastrados são: ");
+        foreach (var numero in setNumeros)
+        {
+            Console.Write($"{numero} ");
+        }
+    }
+}
+
