95b8b98035ffea47775912625c4f2701610d6bcb
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-09-01 10:19:27 -0300

    Exercício 6 resolvido

---
+++ IntroducaoPOO/Exercicio6/Model/Curso.cs
@@ -0,0 +1,83 @@
+namespace Exercicio6.Model;
+
+public class Curso
+{
+    private string titulo = string.Empty;
+    private string descricao = string.Empty;
+    private int duracao;
+    private DateOnly dataInicio;
+    private decimal valor;
+    
+    //Método Construtor
+    public Curso(string titulo, string descricao, int duracao, DateOnly dataInicio, decimal valor)
+    {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.duracao = duracao;
+        this.dataInicio = dataInicio;
+        this.valor = valor;
+    }
+    
+    //Métodos Getters e Setters
+    public string GetTitulo()
+    {
+        return titulo;
+    }
+    
+    public void SetTitulo(string titulo)
+    {
+        this.titulo = titulo;
+    }
+    
+    public string GetDescricao()
+    {
+        return descricao;
+    }
+    
+    public void SetDescricao(string descricao)
+    {
+        this.descricao = descricao;
+    }
+    
+    public int GetDuracao()
+    {
+        return duracao;
+    }
+    
+    public void SetDuracao(int duracao)
+    {
+        this.duracao = duracao;
+    }
+    
+    public DateOnly GetDataInicio()
+    {
+        return dataInicio;
+    }
+    
+    public void SetDataInicio(DateOnly dataInicio)
+    {
+        this.dataInicio = dataInicio;
+    }
+    
+    public decimal GetValor()
+    {
+        return valor;
+    }
+    
+    public void SetValor(decimal valor)
+    {
+        this.valor = valor;
+    }
+    
+    //Método para visualizar os dados do curso
+    public void Visualizar()
+    {
+        Console.WriteLine($"\n{this.titulo.ToUpper()}\n" +
+                          "***************************************************\n" +
+                          $"Descrição: {this.descricao}\n" +
+                          $"Duração: {this.duracao} horas\n" +
+                          $"Data início: {this.dataInicio}\n" +
+                          $"Valor: {this.valor.ToString("C")}" +
+                          "\n***************************************************");
+    }
+}
---
+++ IntroducaoPOO/Exercicio6/Program.cs
@@ -0,0 +1,19 @@
+﻿using Exercicio6.Model;
+
+namespace Exercicio6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("BEM-VINDO AO SISTEMA DE VISUALIZAÇÃO DE CURSOS\n" +
+                          "******************************************************");
+        
+        Curso c1 = new Curso("Ciência da Computação", "Curso que explora fundamentos e aplicações da computação, abrangendo programação, algoritmos, estruturas de dados e teoria da computação.", 4000, new DateOnly(2024, 02, 05), 1150);
+        Curso c2 = new Curso("Engenharia de Software", "Curso que ensina o desenvolvimento eficiente e sistemático de software, incluindo planejamento, projeto, implementação e manutenção.", 4000, new DateOnly(2024, 02, 05), 1000);
+        
+        c1.Visualizar();
+        c2.Visualizar();
+    }
+}
+
